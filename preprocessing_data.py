f928f530f1ebd9549af298a67db44fc34bbf8c4d
Luka Podgorsek <lp5796@student.uni-lj.si>
2018-11-10 16:44:54 +0100

    minor code optimization

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -5,6 +5,7 @@ import nltk
 from nltk.corpus import stopwords
 from langdetect import detect
 
+PRESELECTED_GENRES = ['Hip-Hop', 'Pop', 'Metal']
 
 class PreprocessingData:
     def __init__(self, data=None):
@@ -21,7 +22,15 @@ class PreprocessingData:
             raise ValueError('PreprocessingData.data not defined.')
 
         # import data
+        print("importing data")
         df = pd.read_csv(self.data)
+
+        # REDUCE DATA FRAME SIZE
+        print("getting songs from 2015 onward")
+        # select song from 2015 onward
+        df = df.loc[df['year'] >= 2015]
+        # select only genres defined in PRESELECTED_GENRES
+        df = df.loc[df['genre'].isin(PRESELECTED_GENRES)]
 
         # replace new lines
         df = df.replace({'\n': ' '}, regex=True)
@@ -29,21 +38,18 @@ class PreprocessingData:
         # get word count for songs
         df['word_count'] = df['lyrics'].str.split().str.len()
 
-        # select only Hip-Hop, Pop & Rock genres
-        df = df.loc[df['genre'].isin(['Hip-Hop', 'Pop', 'Metal'])]
-        # select song from 2015 onward
-        df = df.loc[df['year'] >= 2015]
-
         # plot words per song
         # sns.violinplot(x=df["word_count"])
         # plt.show()
 
         # use only song with more than 100 words and less than 1000 because of outliers
+        print("selecting songs with 100-1000 words")
         df = df[df['word_count'] >= 100]
         df_clean = df[df['word_count'] <= 1000]
         print(df_clean['word_count'].groupby(df_clean['genre']).describe())
 
         # count song by genre
+        print("counting songs per genre")
         genre = df_clean.groupby(['genre'], as_index=False).count()
         genre2 = genre[['genre', 'song']]
         print(genre2)
@@ -52,6 +58,7 @@ class PreprocessingData:
         # sns.violinplot(x=df_clean["word_count"])
         # plt.show()
 
+        print("cleaning text")
         # remove brackets with text (Ex. [Verse 1])
         df_new = df_clean.replace({'([\[]).*?([\]])': ''}, regex=True)
         # change all text to lower case
@@ -60,6 +67,7 @@ class PreprocessingData:
         df_new['lyrics'] = df_new['lyrics'].str.replace('[^\w\s^\']', '')
 
         # select only english lyrics
+        print("selecting english songs (needs ~10 seconds)")
         for row in df_new.itertuples(index=True, name='Pandas'):
             try:
                 # if detect(getattr(row, "lyrics")[:200]) != 'en' or detect(getattr(row, "lyrics")[200:400]) != 'en':
@@ -72,14 +80,17 @@ class PreprocessingData:
         # df_new = df_new.loc[df_new['language'] == 'en']
 
         # lemmatization of text & remove stop words
+        print("lemmatization of text & remove stop words")
         df_new['text_lemmatized'] = df_new.lyrics.apply(self.lemmatize_text)
         """
         # remove english stop words
         stop = stopwords.words('english')
         df_new['text_lemmatized_without_stop_words'] = df_new['text_lemmatized'].apply(lambda x: [item for item in x if item not in stop])
         """
+        print("saving processed data to file")
         df_new.to_csv('./input/lyrics_clean_2015_2016.csv')
         # df_new.to_csv('./input/lyrics_test.csv')
+        print("done")
         return df_new
 
 
